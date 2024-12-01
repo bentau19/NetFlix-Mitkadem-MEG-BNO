491c065b507d114e874636908ee3ec95173303f3
ockaa <oscarstilogalo@gmail.com>
2024-12-01 17:09:53 +0200

    Allworks with tests

---
+++ src/File_Classes/BaseFile.h
@@ -0,0 +1,48 @@
+#ifndef BASEFILE_H
+#define BASEFILE_H
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include "IFile.h"
+
+class BaseFile : public IFile {
+protected:
+    std::fstream file;
+    std::string fileName;
+private:
+    std::string loc;
+public:
+    // Constructor: Initialize the file name
+    BaseFile(const std::string name);
+
+    // Destructor: Close the file if it's open
+    virtual ~BaseFile() override;
+
+    // Create a file
+    void create(const std::string name) override;
+
+    // Open the file in the specified mode
+    void openFile(std::ios::openmode mode) override;
+
+    // Delete a file
+    void deleteItem() override;
+
+    // Display file content
+    std::string display() override;
+
+    // Check if a file exists
+    bool doesFileExist() override;
+
+    // Read file content and return as a string
+    std::vector<std::string> read() override;
+
+    // Write to the file
+    void Write(const std::string line) override;
+
+    // Get the file name
+    std::string GetName() const override;
+};
+
+#endif // BASEFILE_H
