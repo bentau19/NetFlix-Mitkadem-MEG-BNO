1fd1e78e3121b50c321b068a89d768e5284ca93e
Ben Tau <62897867+bentau19@users.noreply.github.com>
2024-12-16 13:01:04 +0200

    add patch get post and their tests

--- src/Commands/General/Validity.h
+++ src/Commands/General/Validity.h
@@ -28,6 +28,7 @@ class Validity {
 public:
     static std::vector<std::string> SplitToDigitVec (const std::string& str){
         return StringHandler::splitString(str);
+
     }
     static std::vector<std::string> twoNumsVec(std::string str){
         std::vector<std::string> data = SplitToDigitVec(str);
