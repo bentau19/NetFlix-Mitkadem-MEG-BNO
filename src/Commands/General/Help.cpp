9ce84dbf34ac4210176623aae73626bd6601cc36
Ben Tau <62897867+bentau19@users.noreply.github.com>
2024-12-17 23:43:54 +0200

    Add files via upload

---
+++ src/Commands/General/Help.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include "HelpCommand.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+    Help::Help(){}
+
+    Help::~Help(){}
+std::string Help::execute(std::string str){
+        if(str.length() > 0 || str==" ") { //if got more agument then "help" then invalid
+            throw std::invalid_argument("");
+        } else {
+            //help command
+        cout << "add[userid] [movieid1] [movieid2]…" << endl;
+        cout << "recommend[userid] [movieid]" <<endl;
+        cout << "help" <<endl;
+        }
+    return "";
+    }
+
---
+++ src/Commands/General/HelpCommand.h
@@ -0,0 +1,14 @@
+#ifndef HelpCommand_H
+#define HelpCommand_H
+using namespace std;
+#include <string>
+#include "ICommand.h"
+
+class Help : public ICommand { // Ensure public inheritance
+public:
+    Help();
+    ~Help();
+    virtual std::string execute(std::string str) override; // Use "override" keyword
+};
+
+#endif
