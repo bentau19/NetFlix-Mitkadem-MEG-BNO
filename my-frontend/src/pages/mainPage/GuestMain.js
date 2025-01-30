cead2a43fe0b618fe69a2b0f1f5c097b588f0c93
ockaa <oscarstilogalo@gmail.com>
2025-01-30 13:43:43 +0200

    popupnowork

--- my-frontend/src/pages/mainPage/GuestMain.js
+++ my-frontend/src/pages/mainPage/GuestMain.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from "react-router-dom";
 
+import { get } from '../../components/httpUtils';
+import GuestScreenTemp from '../../components/mainScreen/GuestScreenTemp';
 const GuestMain = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const handleSubmit = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+  
+    try {
+      const result = await get('/movies/search/'); // Remove `{}` unless needed
+      if (!result || result.length === 0) {
+        setError('No movies found for your search.');
+        setData([]); 
+      } else {
+        setData(result);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError('An error occurred while fetching data.');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [setData, setIsLoading, setError]); // Include dependencies
+  useEffect(() => {
+    handleSubmit();
+  }, [handleSubmit]);
+
 
   return (
 
@@ -26,6 +54,11 @@ const GuestMain = () => {
           <li>Accessible from anywhere</li>
         </ul>
       </div>
+      <GuestScreenTemp
+        isLoading={isLoading}
+        error={error}
+        data={data}
+      />
     </div>
   );
 };
